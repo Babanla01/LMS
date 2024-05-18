5a10b8de9c67314cca6af58ddcd4edb1c5f60763
abdulquadri <awonugagbemileke089@gmail.com>
2024-05-18 01:32:49 +0100

    Add script.js with a simple console log

---
+++ script.js
@@ -0,0 +1,27 @@
+      const inputBox = document.querySelector("#input-box");
+      const listContainer = document.querySelector("#book-list");
+      function addBook() {
+        if (inputBox.value === "") {
+          alert("you must add a task First!");
+        } else {
+          let li = document.createElement("li");
+          li.innerHTML = inputBox.value;
+          listContainer.appendChild(li);
+          let span = document.createElement("span");
+          span.innerHTML = "X";
+          li.appendChild(span);
+        }
+        inputBox.value = "";
+      }
+      // const spans = document.querySelector("span");
+      listContainer.addEventListener("click", function (e) {
+        if (e.target.tagName === "LI") {
+          e.target.classList.toggle("checked");
+        } else if (e.target.tagName === "SPAN") {
+          e.target.parentNode.remove();
+          // li.remove();
+        }
+      });
+      // inputBox.addEventListener("keypress", function (e) {
+
+    
